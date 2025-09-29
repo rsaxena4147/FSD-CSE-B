803af9de26eee2cece9ccdeb3cc85f3a8507034f
Rimjhim <rimjhimsaxena535@gmail.com>
2025-09-29 15:35:21 +0530

    upload2

--- javaScript/jsdemo.js
+++ javaScript/jsdemo.js
@@ -3,3 +3,28 @@ const square = num.map((n) => n * n);
 const even = num.filter((n) => n % 2 == 0).map((n) => n * n).reduce((acc, n) => acc + n, 0);
 console.log(even)
 
+
+//destructuring
+let n = [1, 2, 3, 4];
+const [a, b, ...rest] = n;
+console.log(a)
+console.log(b)
+console.log(rest)
+
+let obj = {
+  Name: "Rimjhim",
+  email: "rim@gmail.com",
+  roll: 12,
+  branch: "CSE"
+  
+
+}
+const { Name: newname, email, ...x } = obj;
+
+console.log(newname)
+console.log(email)
+console.log(x)
+
+let arr = [1, 2];
+let brr = [...arr, 5, 6]
+console.log(brr)
